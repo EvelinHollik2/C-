a8fddc49f361ed602d90baecf949f55f9e996a17
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-02-06 11:55:08 +0100

    Add files via upload

---
+++ Run/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Run
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                Console.WriteLine("Ezt a paramétert kaptam: {0}", args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Nins megadva paraméter");
+            }
+            Console.ReadKey();
+        }
+    }
+}

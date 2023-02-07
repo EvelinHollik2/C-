fe2a58e06f4d64ff8a22ad51fcaad25478007277
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-02-07 14:42:45 +0100

    Add files via upload

---
+++ Házi 2023.02.09/Házi_8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Házi_8
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+            int randomNum = random.Next(5, 15);
+            Console.Write("Elvetett búza mennyisége (Tonna): ");
+            double elvetett_buza = Convert.ToDouble(Console.ReadLine());
+            double hozam = elvetett_buza * randomNum;
+            if (randomNum >= 5 && randomNum <= 8)
+                Console.WriteLine($"A következő év átlag alatti lesz. Várható hozam: {hozam} Tonna.");
+            if (randomNum >= 9 && randomNum <= 12)
+                Console.WriteLine($"A következő év átlagos lesz. Várható hozam: {hozam} Tonna.");
+            if (randomNum >= 13 && randomNum <= 15)
+                Console.WriteLine($"A következő év átlag felettti lesz. Várható hozam: {hozam} Tonna.");
+            Console.ReadKey();
+        }
+    }
+}

17c0cb2971d6553fd825623d28f3d7c1be3f241d
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-02-13 19:29:20 +0100

    Add files via upload

---
+++ Házi 2023-02-13/stringek példe_3 keresés/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringek_példe_3_keresés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "verylonglongstring";
+            char[] chs = new char[] { 'y', 'z', 'o' };
+            Console.WriteLine(s.IndexOf('r')); // 2
+            Console.WriteLine(s.IndexOfAny(chs)); // 3
+            Console.WriteLine(s.LastIndexOf('n')); // 16
+            Console.WriteLine(s.LastIndexOfAny(chs)); // 9
+            Console.WriteLine(s.Contains("long")); // true
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Házi 2023-02-13/stringekk példa_4 módosítás/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringekk_példa_4_módosítás
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "smallstring";
+            char[] chs = new char[] { 's', 'g' };
+            Console.WriteLine(s.Replace('s', 'l')); // lmallltring
+            Console.WriteLine(s.Trim(chs)); // mallstrin
+            Console.WriteLine(s.Insert(0, "one")); // onesmallstring
+            Console.WriteLine(s.Remove(0, 2)); // allstring
+            Console.WriteLine(s.Substring(0, 3)); // sma
+            Console.WriteLine(s.ToUpper()); // SMALLSTRING
+            Console.WriteLine(s.ToLower()); // smallstring
+            Console.ReadKey();
+        }
+    }
+}

ef722f72472c0abba3737ca4339e7c250e0b968f
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-02-13 13:23:57 +0100

    Add files via upload

--- 2023.02.09/2023-02-09 Házi_9/Program.cs
+++ 2023.02.09/2023-02-09 Házi_9/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Házi_9
+namespace _9.Feladat
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            float a, b, c;
+            Console.WriteLine("Add meg a C2 és az O2");
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            c = (a / b);
+            Console.WriteLine(c);
+            if (0.8 < c)
+            {
+                Console.WriteLine("Szénhidrátokból nyeri");
+            }
+            else if (0.8 > c)
+            {
+                Console.WriteLine("Zsírból nyeri");
+            }
+            else
+                Console.WriteLine("Tökéletes");
+            Console.ReadKey();
         }
     }
 }

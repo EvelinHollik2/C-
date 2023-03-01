521114a631349f90e416e3401ac65208e877b75d
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-03-01 13:23:58 +0100

    Add files via upload

---
+++ StringMetods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMetods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Ez egy szöveg!";
+            string s2 = "Ez egy szöveg!";
+            string s3 = "Ez egy másik szöveg!";
+
+            bool egyenlo = false;
+
+            egyenlo = String.IsNullOrEmpty(s1);
+            if (egyenlo)
+                Console.WriteLine("Az s1 string üres");
+            else
+                Console.WriteLine("Az s1 string nem üres");
+            
+            egyenlo = String.Compare(s1, s2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Teglalap Evelin/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace Teglalap
+    {
+    internal class program
+    {
+
+    
+        class Teglalap
+        {
+            private int a;
+            private int b;
+            public Teglalap(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+            public int terulet()
+            {
+                return (this.a * this.b);
+            }
+            public int kerulet()
+            {
+                return (2 * (this.a + this.b));
+            }
+            public Teglalap() {}
+            public void setA(int a)
+            {
+                this.a = a;
+            }
+            public void setB(int b)
+            {
+                this.b = b;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Teglalap teglalap = new Teglalap();
+            Console.WriteLine("Add meg a téglalap egyik oldalát: ");
+            teglalap.setA(24);
+            Console.WriteLine("Add meg a téglalap másik oldalát: ");
+            teglalap.setB(3);
+            Console.WriteLine("A téglalap területe: {0}, kerülete: {1}", teglalap.terulet(), teglalap.kerulet());
+            Teglalap teglalap2 = new Teglalap(24, 3);
+            Console.WriteLine("A téglalap területe: {0}, kerülete: {1}", teglalap.terulet(), teglalap.kerulet());
+
+            Console.ReadKey();
+            
+        }
+    }
+}

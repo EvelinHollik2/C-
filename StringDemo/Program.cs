a16ae025100323c63dd88a88716fb95c996ee415
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-03-01 13:22:34 +0100

    Add files via upload

---
+++ StringDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Ez egy szöveg";
+            Console.Write("Kiiratás string-ként: ");
+            Console.WriteLine(s);
+            Console.WriteLine("Kiiratás tömb elemenként: ");
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.Write(s[i]);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ new prog/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace new_prog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tömb deklarálása
+            int hossz = 10; //tömb mérete
+            int alsoHatar = 10, //generált számok alsó határa
+                felsoHatar = 1000; //generált számok felső határa
+
+            int[] tomb = new int[hossz];
+            Random rnd= new Random();
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rnd.Next(alsoHatar, felsoHatar);
+            }
+            //Tömb kiiratása
+            for (int i = 0; i < tomb.Length; i++)
+                Console.WriteLine(tomb[i]);
+
+            //tömb kiiratása máshogy
+            foreach (int i in tomb)
+                Console.WriteLine(i);
+
+
+            Console.ReadKey();
+        }
+    }
+}

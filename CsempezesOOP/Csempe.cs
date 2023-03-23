1738c895361bc95cddc7ffca9c67436171d07476
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-03-23 10:31:00 +0100

    Add files via upload

---
+++ CsempezesOOP/Csempe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsempezesOOP
+{
+    internal class Csempe
+    {
+        private float adat;
+        private float a;
+        private float b;
+        private float cs;
+        private float T; 
+        private float TT;
+
+        public Csempe() { }
+        public Csempe(float adat) 
+        {
+            this.adat = adat;
+        }
+        public float getAdat() 
+        {
+            return this.adat;
+        }
+        public void setAdat(float adat)
+        {
+            this.adat = adat;
+            T = a * b;
+            cs = (float)(T / (0.2 * 0.2));
+            TT = (float)(cs * 1.1);
+            Console.WriteLine(TT);
+
+        }
+
+    }
+}
---
+++ CsempezesOOP/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsempezesOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Csempe c1= new Csempe();
+            Console.WriteLine("Add meg a terület szélességét");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Add meg a terület magasságát");
+            int b = Convert.ToInt32(Console.ReadLine());
+            c1.setAdat(a);
+            c1.setAdat(b);
+
+            Console.ReadLine();
+        }
+    }
+}

e9f75cd4a5e2b5b45cdb046b00c4dcb08efa367a
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-03-27 10:43:10 +0200

    Update csomagoloOOP.cs

--- Csomagolo_ceg/csomagoloOOP.cs
+++ Csomagolo_ceg/csomagoloOOP.cs
@@ -8,14 +8,41 @@ namespace Csomagolo_ceg
 {
     internal class csomagoloOOP
     {
-            Dinnye h1 = new Dinnye();
-            Console.WriteLine("Add meg az átmérőt (CM) :");
-            h1.setD(int.Parse(Console.ReadLine()));
-            Console.WriteLine("Add meg a darabszámot:");
-            h1.setDB(int.Parse(Console.ReadLine()));
-            h1.setOsszeg();
-            Console.WriteLine("A Dinnyékhez ennyi {0} méter szalagra lesz szükséged", h1.getOsszeg());
-            Console.ReadKey();
+           public double osszeg;
+        public double d;
+        public double db;
+
+        public Dinnye() { }
+
+        public Dinnye(double db, double d, double osszeg)
+        {
+            this.db = db;
+            this.d = d;
+            this.osszeg = osszeg;
+        }
+        public void setD(double d)
+        {
+            this.d = d;
+        }
+        public void setDB(double db)
+        {
+            this.db = db;
+        }
+        public double getDB()
+        {
+            return this.db;
+        }
+        public double getD()
+        {
+            return this.d;
+        }
+        public void setOsszeg()
+        {
+            this.osszeg = ((((this.d * Math.PI) * 2) + 60) * this.db) / 100;
+        }
+        public double getOsszeg()
+        {
+            return this.osszeg;
         }
     }
 }

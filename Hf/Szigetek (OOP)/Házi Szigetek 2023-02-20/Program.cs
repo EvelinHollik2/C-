ca32f9515057c1575feea4f9e35072f39981cd1d
EvelinHollik2 <120447128+EvelinHollik2@users.noreply.github.com>
2023-03-27 13:08:09 +0200

    Add files via upload

---
+++ Hf/Szigetek (OOP)/Házi Szigetek 2023-02-20/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace házi_stringek_szigetek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "010110101010100101011110101000"; //szigetek száma 11, leghosszabb sziveg: 4
+            int islandCount = 0; //szigetek száma
+            int maxIslandLength = 0; //legnyasgobb sziget hossza
+            int i = 0; //vízről szigetre lép
+            while (i < data.Length) 
+            {
+                if (data[i] == '1')
+                {
+                    ++islandCount;
+                    int j = i;
+                    int tmp = 0; //sziget beljesét gyűjti
+                    while (j < data.Length && data[j] == '1')
+                    {
+                        ++j;
+                        ++tmp;
+                    }
+                    i = j;
+                    if (tmp > maxIslandLength) { maxIslandLength = tmp; }
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+            Console.WriteLine("A leghosszabb egybefüggő szárazföld hossza: {0}", maxIslandLength);
+            Console.WriteLine("Szigetek száma: {0}", islandCount);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hf/Szigetek (OOP)/Szigetszamolas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szigetek__OOP_
+{
+    internal class Szigetszamolas
+    {
+        // osztályváltozók
+        int islandCount = 0;
+        int maxIslandLength = 0;
+        int i = 0;
+        string data = string.Empty;
+        
+        //konstruktorok
+        public Szigetszamolas() { }
+        public Szigetszamolas(string data) 
+        {
+            this.data = data;
+        }
+
+        //metódusok
+        public void setData(string data) { this.data = data;}
+        public string getData() { return this.data;}
+
+        public int getIslandCount() { return islandCount; }
+        public int getMaxIslandLength() { return maxIslandLength; }
+
+        public void Szamol()
+        {
+            while (i < this.data.Length)
+            {
+                if (this.data[i] == '1')
+                {
+                    ++this.islandCount;
+                    int j = i;
+                    int tmp = 0; //sziget beljesét gyűjti
+                    while (j < this.data.Length && this.data[j] == '1')
+                    {
+                        ++j;
+                        ++tmp;
+                    }
+                    i = j;
+                    if (tmp > this.maxIslandLength) { this.maxIslandLength = tmp; }
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+
+        }
+
+    }
+}
